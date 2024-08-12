229153b8cbf8650759ae02ecdbca7cd86be530d6
Carlos Lopez <carlosalfredolopez96@gmail.com>
2024-08-12 00:40:27 -0600

    save point: first steps of the lexer

--- Project1/src/main/java/main.java
+++ Project1/src/main/java/main.java
@@ -1,5 +1,10 @@
+import frontend.MainWindow;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create a new instance of the main window
+        MainWindow mainWindow = new MainWindow();
+        // Show the main window
+        mainWindow.setVisible(true);
     }
 }
